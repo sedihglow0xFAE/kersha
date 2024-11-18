7bc1125806e72aab00cbb623f3aaeda2969f1426
sedihglow0xFAE <sedihglow0xFAE@gmail.com>
2024-11-18 14:08:34 -0800

    Mah bathtub in mah bathroom
    heheheheheh witchy witchy.

--- Mah future room, with mah furture witch.c
+++ Mah future room, with mah furture witch.c
@@ -5,8 +5,19 @@ See, if guys have a room like this, guys call you gay, but when a girl walks in
 your laid instantly. Why cant we have these things?
 https://www.pinterest.com/pin/40954677856973349/
 
-
-
 See, this is the kind of person i would date, the purple witch with
 the green bar. <3 LOVE YOU /bKERSHA/b AND MAH CRONE MOTHER AND MAH CRONE CRONE.
 https://www.pinterest.com/pin/25262447904021152/
+
+For those saying these rooms are not real because they look so good, look
+at the bathtub that would get you boys called gay so you would never have one
+like it, but men like me, sit in the tub with the bitches getting laid in rooms like
+this. These are the things i like and one of the many many reasons other than being
+confident in myself that make me not care if i get called gay by some dumb fuck. :) <3
+
+https://www.pinterest.com/pin/470415123595635473/
+
+For some reason these aesthetics and types of houses and rooms and yards are a reason why
+christians think witches are evil and need to be burned, drowned or lynched. GOOD THOUGHTS AND
+ACTIONS RETARDS. Stop sending your fucking people and your no name bitch ass things to kill me and
+my spirits and friends so i can sit in my bathtub. Thanks.
