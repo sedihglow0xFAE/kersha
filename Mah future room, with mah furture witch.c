96245d6715eb6b596ebee4150597a73ba85e6aab
sedihglow0xFAE <sedihglow0xFAE@gmail.com>
2024-11-18 14:01:36 -0800

    Rooms that get guys called gay, so they dont...
    This is what i mean, guys are too afraid of being called gay to have a room like this. I get laid. They dont.

---
+++ Mah future room, with mah furture witch.c
@@ -0,0 +1,12 @@
+This is what i mean, guys are too afraid of being called gay to have a room like this. I get laid. They dont.
+
+Why cant i have a room ;like this.
+See, if guys have a room like this, guys call you gay, but when a girl walks in
+your laid instantly. Why cant we have these things?
+https://www.pinterest.com/pin/40954677856973349/
+
+
+
+See, this is the kind of person i would date, the purple witch with
+the green bar. <3 LOVE YOU /bKERSHA/b AND MAH CRONE MOTHER AND MAH CRONE CRONE.
+https://www.pinterest.com/pin/25262447904021152/
