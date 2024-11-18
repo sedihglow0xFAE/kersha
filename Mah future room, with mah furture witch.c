2e7a05949aa323fa3d3dc18550f82728c07e92cc
sedihglow0xFAE <sedihglow0xFAE@gmail.com>
2024-11-18 15:26:57 -0800

    Update, update.

--- Mah future room, with mah furture witch.c
+++ Mah future room, with mah furture witch.c
@@ -69,7 +69,7 @@ https://www.pinterest.com/pin/b-o-o-in-2024--8936899256614355/
 
 This is what i mean, guys are too afraid of being called gay to have a room like this. I get laid. They dont.
 
-Why cant i have a room ;like this.
+Why cant i have a room like this.
 See, if guys have a room like this, guys call you gay, but when a girl walks in
 your laid instantly. Why cant we have these things?
 https://www.pinterest.com/pin/40954677856973349/
@@ -77,6 +77,7 @@ https://www.pinterest.com/pin/40954677856973349/
 See, this is the kind of person i would date, the purple witch with
 the green bar. <3 LOVE YOU /bKERSHA/b AND MAH CRONE MOTHER AND MAH CRONE CRONE.
 https://www.pinterest.com/pin/25262447904021152/
+https://www.pinterest.com/pin/43910165111154549/
 
 For those saying these rooms are not real because they look so good, look
 at the bathtub that would get you boys called gay so you would never have one
@@ -90,7 +91,6 @@ For some reason these aesthetics and types of houses and rooms and yards are a r
 christians think witches are evil and need to be burned, drowned or lynched. GOOD THOUGHTS AND
 ACTIONS RETARDS. Stop sending your fucking people and your no name bitch ass things to kill me and
 my spirits and friends so i can sit in my bathtub. Thanks.
-
   
 And this one is for the minis.
 https://www.pinterest.com/pin/413697915789471162/
@@ -107,8 +107,10 @@ https://www.pinterest.com/pin/140806233189692/
 https://www.pinterest.com/pin/3518505953957884/
 https://www.pinterest.com/pin/1196337402530287/
 https://www.pinterest.com/pin/4785143350406789/
+https://www.pinterest.com/pin/3166662231455564/
 https://www.pinterest.com/pin/4292562139442990/ mini prince? WHO ARE YOU
 https://www.pinterest.com/pin/1759287347783491/ Shadow minis?
+https://www.pinterest.com/pin/8655424280817222/
 
 Fae minis,
 https://www.pinterest.com/pin/860680178821013911/
@@ -121,8 +123,6 @@ https://www.pinterest.com/pin/imagens-in-2024--19140367161583823/
 
 What region do you think this type of witch lives in?
 https://www.pinterest.com/pin/1266706140095584/
-
-https://www.pinterest.com/pin/43910165111154549/
 
 Then she showed her age. I knew she knew at least some sorcerressy
 https://www.pinterest.com/pin/774124930181862/
