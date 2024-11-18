5e6ab18407db13308af12365c8b6e13e9b551826
sedihglow0xFAE <sedihglow0xFAE@gmail.com>
2024-11-18 15:01:38 -0800

    Best update yet. Good intel.

--- Mah future room, with mah furture witch.c
+++ Mah future room, with mah furture witch.c
@@ -1,6 +1,34 @@
 From the chat -
 "See, thats James" - the chat
 https://www.pinterest.com/pin/1759287347783467/
+https://www.pinterest.com/pin/surreal-fever-dreams-by-klaartje-lambrechts--321233385929549420/
+
+Why do people call me gay and why do i love womens clothing? I hope this clears things up.
+https://www.pinterest.com/pin/184999497188811183/
+https://www.pinterest.com/pin/1125968651947034/
+https://www.pinterest.com/pin/22447698137545751/
+https://www.pinterest.com/pin/3870349674208909/
+https://www.pinterest.com/pin/19210735904419315/
+https://www.pinterest.com/pin/ren-faire-fae-in-2024--68748068269/
+https://www.pinterest.com/pin/19210735904419572/
+https://www.pinterest.com/pin/if-the-met-gala-participants-had-truly-embraced-the-theme-of-garden-of-time-this-is-what-they-could-have-looke-in-2024--635852041174726081/
+https://www.pinterest.com/pin/6192518231721607/
+https://www.pinterest.com/pin/703756187557520/
+https://www.pinterest.com/pin/2814818511480840/
+https://www.pinterest.com/pin/3870349674212624/
+https://www.pinterest.com/pin/12384967722693494/
+https://www.pinterest.com/pin/151574343703958364/
+https://www.pinterest.com/pin/703756187656300/
+https://www.pinterest.com/pin/1407443628752693/
+https://www.pinterest.com/pin/1407443628962607/ Underware
+
+Why you think we fly high like a butterfly? To get shit like this.
+https://www.pinterest.com/pin/703756187557520/
+https://www.pinterest.com/pin/211174977658986/ The real thing.
+https://www.pinterest.com/pin/492649953321463/
+
+Sorry sir, there's just nothing we could do, these clothes are
+too powerfull.
 
 When my bitch (dont kill me) who is not my bitch (maybe i wont die if i say that)
 is wearing colors.
@@ -12,6 +40,10 @@ https://www.pinterest.com/pin/18084835997689445/
 
 Spectral
 https://www.pinterest.com/pin/16958936095662380/
+https://www.pinterest.com/pin/70437489391233/
+I told you down below, we do glass art too.
+https://www.pinterest.com/pin/351912465723798/
+https://www.pinterest.com/pin/1012817403696090340/
 
 Sorry sir, nothings sexier than a NW witch.
 (Still need to refind that NW witch of purple dark variety)
@@ -61,6 +93,15 @@ https://www.pinterest.com/pin/rustic-cinnamon-broom--570268371581879727/
 
 For the MINIS.
 https://www.pinterest.com/pin/4362930883172878/
+https://www.pinterest.com/pin/140806233189692/
+https://www.pinterest.com/pin/3518505953957884/
+https://www.pinterest.com/pin/1196337402530287/
+https://www.pinterest.com/pin/4785143350406789/
+https://www.pinterest.com/pin/4292562139442990/ mini prince? WHO ARE YOU
+https://www.pinterest.com/pin/1759287347783491/ Shadow minis?
+
+Fae minis,
+https://www.pinterest.com/pin/860680178821013911/
 
 We go glass art too, full circle.
 https://www.pinterest.com/pin/halloween-witch-stained-glass-skeleton-pumpkin-crow-etsy-in-2024--155303888161164286/
@@ -74,8 +115,34 @@ https://www.pinterest.com/pin/1266706140095584/
 Then she showed her age. I knew she knew at least some sorcerressy
 https://www.pinterest.com/pin/774124930181862/
 
-Mother crone pissed she has to wear purple for kersha day. Shes more of a blue girl.
+Mother crone pissed she has to wear purple for kersha day.
 https://www.pinterest.com/pin/67976275686042607/
+Shes more of a blue girl.
+https://www.pinterest.com/pin/10133167905323395/
+https://www.pinterest.com/pin/531072981078232425/
+What you think she wears under that dress? So you dont peek. (or so you do?)
+https://www.pinterest.com/pin/1196337403248792/
 
 A young crone.
 https://www.pinterest.com/pin/281543724462706/
+https://www.pinterest.com/pin/4785143350406789/
+
+See, if greeks wore this, then they would be closer to being as hot as a
+NW witch.
+https://www.pinterest.com/pin/8655424280801164/
+https://www.pinterest.com/pin/5066618329449300/
+
+Same goes for queen elizibeth
+https://www.pinterest.com/pin/1548181174516104/
+
+They called her 4 eyes,
+https://www.pinterest.com/pin/155303888196563169/
+but then it turned out it was just the glasses.
+https://www.pinterest.com/pin/pines-creados-por-ti-in-2024--68748034340/
+https://www.pinterest.com/pin/in-2024--2322237301624573/
+
+Native ass witch shit
+https://www.pinterest.com/pin/422281210990739/
+
+I wonder why spiders like me.
+https://www.pinterest.com/pin/469781804899628368/
